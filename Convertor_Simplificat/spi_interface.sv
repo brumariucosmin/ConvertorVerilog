fe5279ed445dc35aa83a20a3d6adb5e461cef630
Vlad25042001 <solomonvlad2001@yahoo.com>
2025-03-22 22:28:13 +0200

    Create spi_interface.sv
    SPI_interface

---
+++ Convertor_Simplificat/spi_interface.sv
@@ -0,0 +1,19 @@
+`ifndef __spi_intf
+`define __spi_intf
+
+interface spi_interface_dut;
+  logic  sclk; 
+  logic  mosi;
+  logic  miso;
+  logic  cs;
+  logic  spi_done
+;
+  
+ import uvm_pkg::*;
+      
+//ASERTII
+      
+endinterface
+
+
+`endif

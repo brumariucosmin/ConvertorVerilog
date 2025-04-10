4ffd1c4487a441237607385d2d9d52c48d0b5d4b
DragosBajenescu <95871902+DragosBajenescu@users.noreply.github.com>
2025-04-10 18:06:19 +0300

    Add files via upload

--- Convertor_Simplificat/design.sv
+++ Convertor_Simplificat/design.sv
@@ -1,9 +1,187 @@
-module apb_slave(pclk_i, rst_n_i, psel_i, penable_i, paddr_i);
+`include "counter.sv"
 
-  input pclk_i;
-  input rst_n_i;
-  input [2:0] paddr_i;
-  input psel_i;
-  input penable_i;
+module converter #(parameter bit[7:0] NO_OF_SPI_BITS = 8)(clk, rst_n, paddr, pwdata, psel, penable, miso, pwrite, pready, pslverr, prdata, mosi, old_cs, sclk);
 
+  input logic clk;
+  input logic rst_n;
+  input logic [2:0] paddr;
+  input logic [7:0] pwdata;
+  input logic psel;
+  input logic penable;
+  input logic miso;
+  input logic pwrite;
+  
+  output logic pready;
+  output logic pslverr;
+  output logic[7:0] prdata;
+  output logic mosi;
+  output logic old_cs;
+  output logic sclk;
+	
+  reg [7:0] reg_operand, reg_result, reg_control; //adresele: 0, 2, 4
+  reg [2:0] cnt_spi;
+  
+  reg [7:0] count_out;
+  reg count_up_down;
+  reg enable;
+  reg load;
+  reg terminate_cnt;
+  reg posedge_sclk, negedge_sclk, cs_delayed;
+  reg sclk_delayed;
+  reg cs;
+  
+  counter counter_inst(
+  .clk (clk),
+  .rst_n(rst_n),
+  .load (load),
+  .enable (enable),
+  .count_up_down (1'b0),
+  .data_in (NO_OF_SPI_BITS-1),
+  .count_out (count_out),
+  .terminate_cnt (terminate_cnt)
+  );
+  
+	always @(posedge clk or negedge rst_n)
+		if(~rst_n)
+			sclk <= 0;
+		else if(old_cs == 0)
+			sclk <= ~sclk;
+			else
+			sclk <= 0;
+			
+  always @(posedge clk or negedge rst_n)//read
+	if(~rst_n)
+		prdata<=0;
+		else
+		if(psel && !penable && !pwrite)
+		case(paddr)
+		0:prdata<= reg_operand;
+		2: prdata<= reg_result;
+		4: prdata<= reg_control;
+		default: $display("unnalocatted address");
+		endcase
+	always @(posedge clk or negedge rst_n)//write
+		if(~rst_n)begin
+			reg_operand<=0;
+			reg_result<= 0;
+			reg_control<=0;
+			end
+			else if(psel && !penable && pwrite)
+			case(paddr)
+			0:begin
+			reg_operand <= pwdata;
+			reg_result <= pwdata ^ (pwdata >> 1);
+			end
+		//	2: reg_result<= pwdata;
+			2: $warning("read_only_register");
+			4:reg_control <= pwdata;
+			default: $display("unnalocatted address");
+			endcase
+			
+			
+			
+	always @(posedge clk or negedge rst_n)//write
+		if(~rst_n)begin
+			reg_control<=0;
+			end
+			else if(psel && !penable && pwrite && paddr ==4)
+			reg_control <= pwdata;
+			else 
+			    begin 
+				   if(reg_control[0] == 1'b1)  //start
+					    reg_control[0] <= 1'b0; //reset start
+					if(reg_control[1] == 1'b1)  //stop
+					   reg_control[1] <= 1'b0; //reset stop
+					   
+				 //  reg_control[1] <= old_cs;
+				 if(reg_control[1] == 1 && terminate_cnt==0)
+				 reg_control[7] <= 1;
+			    end
+				
+							
+	always @(posedge clk or negedge rst_n)//old_cs
+		if(~rst_n)
+			old_cs<= 1;
+			else if(reg_control[0] == 1)
+			old_cs <= 0;
+			else if(reg_control[1] == 1 || count_out == 0)
+			old_cs <= 1;
+			
+			
+		always @(posedge clk or negedge rst_n)//cs_delayed
+		if(~rst_n)
+			cs_delayed<= 1;
+			else
+			cs_delayed <= old_cs;
+			
+			assign cs = cs_delayed & old_cs;
+			
+			assign enable = (~old_cs & negedge_sclk) | load;//cand old_cs nu este activat, nici counter-ul nu numara
+			
+	always @(*)//mosi
+	begin
+		 if(old_cs == 0 && posedge_sclk )
+			mosi <= reg_result[count_out];
+			else if (cs ==1)
+			mosi <= 1'bz;
+	end		
+			
+			
+	always @(posedge clk or negedge rst_n)//sclk intarziat
+		if(~rst_n)
+			sclk_delayed<= 0;
+			else 
+			sclk_delayed <= sclk;
+			
+	  assign posedge_sclk = sclk & ~sclk_delayed;
+	  assign negedge_sclk = ~sclk & sclk_delayed;
+	assign load = reg_control[0];
+			
+	always @(posedge clk or negedge rst_n)
+		if(~rst_n)
+			pready<=0;
+			
+			else if (pready == 1)
+			pready <=0;
+			else if(psel && !penable)
+			pready<=1;
+			
+	always @(posedge clk or negedge rst_n)//pslverr (in caz ca nu accesam o adresa corecta)
+		if(~rst_n)
+			pslverr<=0;
+			
+			else if(pslverr == 1)
+			pslverr <= 0;
+			else if (psel && !penable && (!(paddr == 0 ||paddr == 2 || paddr ==4)||(paddr==2 && pwrite ==1)))
+			pslverr <= 1;
+			/*
+	always @(posedge clk or negedge rst_n)
+			if(!rst_n) begin
+			mosi 		<= 1'b0; //nu transmitem nimic
+			old_cs			<= 1'b1; //spi dezactivat
+			end
+			else begin
+				end
+				if(reg_operand < 8'd255) begin // daca nr scris este mai mare decat 255
+					reg_result <= 8'd0;
+					reg_control[1] <= 1'b1;
+					reg_control[7] <= 1'b1;
+					old_cs <= 1'b1;
+				end 
+				else begin
+					cnt_spi <= 3'd7;
+					old_cs <= 1'b0;
+				end
+				if(old_cs == 1'b0) begin
+					mosi<= reg_result[cnt_spi];
+				end
+				if(cnt_spi == 0) begin
+					old_cs <= 1'b1;
+					reg_control[1] <= 1'b1;
+				end 
+				else begin 
+					cnt_spi <= cnt_spi - 1;
+				end
+				end*/
+				
 endmodule
--- Convertor_Simplificat/testbench.sv
+++ Convertor_Simplificat/testbench.sv
@@ -1,12 +1,4 @@
-`include "uvm_macros.svh"
-import uvm_pkg::*;
-
 `define PERIOADA_CEASULUI 10
-
-
-`ifndef 10//numarul minim de tranzactii care se vor genera pe interfata de intrare
-  `define 10 10
-`endif
 
 //`define DEBUG      //parametru folosit pentru a activa mesaje pe care noi le stabilim ca ar fi necesare doar la debug
 
@@ -15,55 +7,126 @@ import uvm_pkg::*;
 
 //includerea fisierelor la care modulul de top trebuie sa aiba acces
 
-`include "apb_interface_dut.sv"
-`include "test_exemplu.sv"
 `include "design.sv"
+`include "counter.sv"
 
 // Code your testbench here
-
 module top();
    logic clk;
-   wire  rst_n;
-   wire  [2:0] paddr;
-   wire         psel;
-   wire         penable;
+   logic rst_n;
+   logic [2:0] paddr;
+   logic psel;
+   logic penable;
+   logic [7:0] pwdata;
+   logic pwrite;
+   logic miso;
+   wire pready;
+   wire mosi;
+   wire cs;
+   wire [7:0] prdata;
+   wire pslverr;
+   wire sclk;
+   
   //sunt create instantele interfetelor (in acest proiect sunt 2 agenti, deci vor fi 2 interfete); se leaga semnalele interfetelor de semnalele din modulul de top
-  apb_interface_dut intf_apb();
-  assign intf_apb.pclk = clk;
-  assign rst_n         = intf_apb.rst_n;
-  assign psel          = intf_apb.psel;
-  assign penable       = intf_apb.penable;
-  assign paddr         = intf_apb.paddr;
+ // apb_interface_dut intf_apb();
+ // assign intf_apb.pclk = clk;
+  //assign rst_n         = intf_apb.rst_n;
+  //assign psel          = intf_apb.psel;
+  //assign penable       = intf_apb.penable;
+  //assign paddr         = intf_apb.paddr;
   
   
   initial begin
     //cele 2 linii de mai jos permit vizualizarea formelor de unda (pentru a vizualiza formele de unda trebuie bifata si optiunea "Open EPWave after run" din sectiunea "Tools & Simulators" aflata in stanga paginii)
-    $dumpfile("dump.vcd");
-    $dumpvars;
+   // $dumpfile("dump.vcd");	
+   // $dumpvars;
     //se genereaza ceasul
-	clk = 1;
+	clk <= 1;
 	forever begin 
     #(`PERIOADA_CEASULUI/2)  
     clk <= ~clk;
   end
 	end
-  
-   initial
-  	begin
-      //se salveaza instantele interfetelor in baza de date UVM
-      uvm_config_db#(virtual apb_interface_dut)::set(null, "*", "apb_interface_dut", intf_apb);
-
-      //se ruleaza testul dorit
-      run_test("test_exemplu");
-  	end
 
   // se instantiaza DUT-ul, facandu-se legaturile intre semnalele din modulul de top si semnalele acestuia
-  apb_slave DUT(
-	.pclk_i                   (clk    ),
-	.rst_n_i                 (rst_n   ),
-	.psel_i                  (psel    ),
-	.penable_i               (penable ),
-  .paddr_i                 (paddr   )
+  converter DUT(
+	.clk (clk),
+	.rst_n (rst_n),
+	.psel (psel),
+	.penable (penable),
+	.paddr (paddr),
+	.pwdata (pwdata),
+	.miso (miso),
+	.pwrite (pwrite),
+	.pready	(pready),
+	.prdata (prdata),
+	.mosi(mosi),
+	.old_cs (old_cs),
+	.pslverr (pslverr),
+	.sclk (sclk)
 );
 
+	
+task write_register(bit [2:0] addr, bit [7:0] data);
+	//T1
+	@(posedge clk);
+	paddr <= addr;
+	pwrite <= 1'b1;
+	psel <= 1'b1;
+	penable<= 1'b0;
+	pwdata <= data;
+	//T2
+	@(posedge clk);
+	penable <= 1'b1;
+	//T3
+	@(posedge clk);
+	psel <= 1'b0;
+	paddr<=1'bz;
+	pwrite<=1'bz;
+	penable<=1'b0;
+	pwdata<='bz;
+endtask
+
+task read_register(bit [2:0] addr);
+	//T1
+	@(posedge clk);
+	paddr <= addr;
+	pwrite <= 1'b0;
+	psel <= 1'b1;
+	penable<= 1'b0;
+	//T2
+	@(posedge clk);
+	penable <= 1'b1;
+	//T3
+	@(posedge clk);
+	psel <= 1'b0;
+	paddr<=1'bz;
+	pwrite<=1'bz;
+	penable<=1'b0;
+endtask
+
+initial begin
+rst_n <= 0;
+paddr <= 0;
+pwdata <= 0;
+psel <= 0;
+penable <= 0;
+miso <= 0; 
+pwrite <= 0;
+
+repeat(5) @(posedge clk);
+rst_n <= 1;
+repeat(5) @(posedge clk);
+write_register(3'd0, 8'h23);
+write_register(3'd4, 8'b00000001);
+read_register(3'd2);
+read_register(3'd0);
+read_register(3'd4);
+
+repeat(5) @(posedge clk);
+write_register(3'd4, 8'b00000010);
+#1000
+$stop();
+ end
+ 
 endmodule
